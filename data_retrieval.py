6fd8304551095a31e5bda85ae5dc72e6f4b56dab
Natansh2004 <minanatansh@gmail.com>
2024-09-21 12:33:47 +0530

    uploading

---
+++ data_retrieval.py
@@ -0,0 +1,12 @@
+import sqlite3
+conn = sqlite3.connect('loan_approval.db')
+cur = conn.cursor()
+
+query = "select * from client_details;"
+cur.execute(query)
+
+for record in cur.fetchall():
+    print(record)
+
+cur.close()
+conn.close()
